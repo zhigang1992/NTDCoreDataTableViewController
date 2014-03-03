ac93d7b535c2e8190104897011b6103d19e75ad8
NicholasTD07 <Nicholas.TD07@gmail.com>
2014-03-03 11:30:45 +0800

    Added import for CoreData to pass CocoaPods' validation.

--- NTDCoreDataTableViewController/NTDCoreDataTableViewController.h
+++ NTDCoreDataTableViewController/NTDCoreDataTableViewController.h
@@ -7,6 +7,11 @@
 
 #import <UIKit/UIKit.h>
 
+// Delete this if you do NOT need it.
+// e.g. You may be have this line in Prefix.pch.
+// Or you may be using MagicalRecord, etc.
+#import <CoreData/CoreData.h>
+
 @interface NTDCoreDataTableViewController : UITableViewController <NSFetchedResultsControllerDelegate>
 
 @property (nonatomic, strong) NSFetchedResultsController *fetchedResultsController;
