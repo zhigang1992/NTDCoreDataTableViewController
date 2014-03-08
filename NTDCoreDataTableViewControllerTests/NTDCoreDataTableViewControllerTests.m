e45f91b7c0d2222fa60d39c52f3dfa5942b372b6
NicholasTD07 <Nicholas.TD07@gmail.com>
2014-03-08 16:42:44 +0800

    Added Tests folder in.

---
+++ NTDCoreDataTableViewControllerTests/NTDCoreDataTableViewControllerTests.m
@@ -0,0 +1,34 @@
+//
+//  NTDCoreDataTableViewControllerTests.m
+//  NTDCoreDataTableViewControllerTests
+//
+//  Created by Nicholas Tian on 3/8/14.
+//  Copyright (c) 2014 Nicholas Tian. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NTDCoreDataTableViewControllerTests : XCTestCase
+
+@end
+
+@implementation NTDCoreDataTableViewControllerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
